055eb233012f7ccf2d279e4dc9b458ac7c711aaf
Yurii <ukarapetanc@gmail.com>
2023-12-28 17:55:31 +0200

    go
    go

--- src/components/Transactions/TransactionHistory.jsx
+++ src/components/Transactions/TransactionHistory.jsx
@@ -3,22 +3,26 @@ import transactions from '../../data/transactions.json';
 import clsx from 'clsx';
 
 export const TransactionHistory = ({ items }) => (
-  <table classList="transaction-history">
-    <thead>
-      <tr>
-        <th>Type</th>
-        <th>Amount</th>
-        <th>Currency</th>
-      </tr>
-    </thead>
-    <tbody>
-      {items.map(({ id, type, amount, currency }) => (
-        <tr key={id}>
-          <td>{type}</td>
-          <td>{amount}</td>
-          <td>{currency}</td>
-        </tr>
-      ))}
-    </tbody>
-  </table>
+  <div className={css.firstCon}>
+    <div className={css.container}>
+      <table classList={css.table}>
+        <thead className={css.thead}>
+          <tr>
+            <th className={css.th}>Type</th>
+            <th className={css.th}>Amount</th>
+            <th className={css.th}>Currency</th>
+          </tr>
+        </thead>
+        <tbody>
+          {items.map(({ id, type, amount, currency }) => (
+            <tr key={id} className={css.tr}>
+              <td className={css.td}>{type}</td>
+              <td className={css.td}>{amount}</td>
+              <td className={css.td}>{currency}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  </div>
 );
